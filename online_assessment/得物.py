430735b7840197105e48e5a37af1d8d2ee6e0d17
Cassia <40779641+FredericaErath@users.noreply.github.com>
2024-01-12 23:54:37 -0800

    add oa

--- online_assessment/得物.py
+++ online_assessment/得物.py
@@ -63,7 +63,7 @@ def solution2(n, m, ls_tree):
     # ls_tree = [map(int, input().split()) for _ in range(m)]
     ls_tree.sort(key=lambda x: -x[2])
     # 并查集
-    fa = [i for i in range(n+1)]
+    fa = [i for i in range(n + 1)]
 
     def get_f(x):
         # 获取父亲节点
@@ -83,9 +83,81 @@ def solution2(n, m, ls_tree):
             ans += w
             cnt += 1
     print(cnt, ans)
-    return ans if cnt == n-1 else "No solution."
+    return ans if cnt == n - 1 else "No solution."
 
 
+"""
+最少数字
+小明用计算机随机生成了N个正整数，他希望从这N个数中选取若干个数，使得它们的和等于M。这些随机生成的数字可能会相同，但是每个数字最多只允许使用一次。
+当然这样的选取方案可能不存在，也可能有多个。
+现在希望你编写一个程序，能够找出数字个数最少的选取方案，输出对应的最少数字的个数，如果无解输出“No solution"。
+输入
+单组输入，每组输入2行
+第1行包含两个正整数N和M，分别表示初始输入的正整数个数和目标数字和(N< 10M105)第2行为N个正整数，两两之间用空格隔开(每一个正整数均小于等于105)。
+输出
+输出数字个数最少的选取方案中所包含的最少数字个数，如果无解输出“No solution”
+样例
+输入:
+5 5
+1 3 2 1 1
+输出:
+2
+"""
+# 还是01背包 略
 
 
+"""
+导演在组织进行大运会开幕式的排练，其中一个环节是需要参演人员围成一个环形。
+演出人员站成了一圈，出于美观度的考虑，导演不希望某一个演员身边的其他人比他低太多或者高太多。
+现在给出n个参演人员的身高，问在他们站成一圈时，相邻演员的身高差的最大值至少是多少? 请你帮忙计算。
 
+输入
+输入包括两行，第一行有1个正整数，代表人数n.
+第二行有n个空格隔开的正整数h;表示第i个演员的身高
+数据保证2<n<10**5,1<hi<10**9
+输出
+输出包括一个正整数，表示答案
+
+样例
+输入:
+5
+2 1 1 3 2
+输出:
+1
+提示:
+排为1，2，3，2，1即可。
+
+输入:
+2
+10 20
+输出:
+10
+提示:
+排为10，20即可。
+"""
+
+
+def solution3(n, heights):
+    if n == 1:
+        return heights[0]
+    if n == 2:
+        return abs(heights[1] - heights[0])
+    heights.sort(reverse=True)
+    # 双指针重构数组，大数在中间
+    min_gap = -1
+    mid1 = mid2 = heights[0]
+    if n % 2 == 0:
+        left, right = 0, 1
+    else:
+        left, right = 1, 2
+    while left < n and right < n:
+        min_gap = max(min_gap, mid1 - heights[left], mid2 - heights[right])
+        mid1 = heights[left]
+        mid2 = heights[right]
+        left += 2
+        right += 2
+    print(min_gap)
+    return min_gap
+
+
+solution3(3, [2, 3, 1])
